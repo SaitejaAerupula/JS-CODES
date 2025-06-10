7211fb1abf64ede4172b835646ae9f2d89279080
Saiteja Aerupula <144018477+SaitejaAerupula@users.noreply.github.com>
2025-06-10 20:43:27 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,8 @@
+
+var a = 1;
+var b = 2;
+var res = a + b;
+document.write("The sum of " + a + " and " + b + " is: " + res);
+document.write("<h1>External</h1>");
+document.write("<p>This is an external JavaScript file.</p>");
+document.write("<p>It is linked to an HTML file.</p>"); 
